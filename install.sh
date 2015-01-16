be5cf4aba107dd424fb01eac46c67ce117231243
Lee Hyeonseop <protos37@gmail.com>
2015-01-16 19:44:27 +0900

    download and install vim vundle

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ do
 	ln -s $(pwd)/$FILENAME ~/.$FILENAME
 done
 cd - > /dev/null
+
+# Download Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Vundle
+vim +PluginInstall +qall
